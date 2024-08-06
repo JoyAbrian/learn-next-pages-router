edd946035315604024ac48ffd2fc639eb54ad5fc
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-06 20:21:07 +0800

    Update index.tsx

--- src/pages/views/product/index.tsx
+++ src/pages/views/product/index.tsx
@@ -11,10 +11,10 @@ type ProductType = {
 const ProductView = ({ products }: {products: ProductType[] }) => {
     return (
         <Fragment>
-            <h1 className="text-2xl font-semibold text-center mt-5">Product List</h1>
-            <div className="grid grid-cols-6 w-screen mx-3 mt-5 gap-x-2 gap-y-4">
+            <h1 className="text-4xl font-semibold text-center mt-5">Product List</h1>
+            <div className="grid grid-cols-5 w-screen mx-3 my-5 gap-y-6 gap-x-4">
                 {products.map((product: ProductType) => (
-                    <div className="w-3/4" key={product.id}>
+                    <div className="w-full h-fit" key={product.id}>
                         <img src={product.image} alt={product.name} />
                         <h3 className="font-semibold">{product.name}</h3>
                         <p className="text-gray-700" >{product.category}</p>

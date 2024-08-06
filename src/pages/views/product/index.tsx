b1ec6190a496b2cd2d4db94eaba2cd7aaa7c22bb
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-06 20:13:31 +0800

    fix product view

--- src/pages/views/product/index.tsx
+++ src/pages/views/product/index.tsx
@@ -12,9 +12,9 @@ const ProductView = ({ products }: {products: ProductType[] }) => {
     return (
         <Fragment>
             <h1 className="text-2xl font-semibold text-center mt-5">Product List</h1>
-            <div className="flex mx-5 mt-5 gap-4">
+            <div className="grid grid-cols-6 w-screen mx-3 mt-5 gap-x-2 gap-y-4">
                 {products.map((product: ProductType) => (
-                    <div className="w-1/5" key={product.id}>
+                    <div className="w-3/4" key={product.id}>
                         <img src={product.image} alt={product.name} />
                         <h3 className="font-semibold">{product.name}</h3>
                         <p className="text-gray-700" >{product.category}</p>

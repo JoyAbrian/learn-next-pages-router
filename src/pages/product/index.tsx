d6ffb28a12de49d47fc4e4563998cc764acb24c6
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-06 19:19:02 +0800

    video 6 API Routes

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,18 +1,36 @@
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
+type ProductType = {
+    id: number;
+    name: string;
+    price: number;
+    stock: number;
+};
 const ProductPage = () => {
     const [ isLogin, setIsLogin ] = useState(false)
+    const [ products, setProducts ] = useState([])
     const { push } = useRouter()
 
+    // useEffect(() => {
+    //     if (!isLogin) {
+    //         push("/auth/login")
+    //     }
+    // }, [])
+
     useEffect(() => {
-        if (!isLogin) {
-            push("/auth/login")
-        }
+        fetch("/api/product")
+            .then(res => res.json())
+            .then((response) => setProducts(response.data))
     }, [])
 
     return (
-        <div>Product Page</div>
+        <div>
+            <h1>Product Page</h1>   
+            {products.map((product: ProductType) => (
+                <div key={product.id}>{product.name}</div>
+            ))}
+        </div>
     )
 }
 

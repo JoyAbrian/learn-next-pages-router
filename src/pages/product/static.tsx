4f0f3acbc1c1535db2cd378ba25aaf040d4024b9
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-06 20:41:44 +0800

    video 9 Static Site Generation

---
+++ src/pages/product/static.tsx
@@ -0,0 +1,26 @@
+import ProductView from "../views/product";
+import ProductType from "../../types/product.type";
+
+const ProductPage = (props: {products: ProductType[]}) => {
+    const { products } = props;
+
+    return (
+        <div>
+            <ProductView products={products} />
+        </div>
+    )
+}
+
+export default ProductPage;
+
+// Dipanggil setiap melakukan request
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:3000/api/product')
+    const response = await res.json()
+
+    return {
+        props: {
+            products: response.data
+        }
+    }
+}

6739abe1a40aa802ffdd354eb213bb74dea808ca
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-06 18:39:28 +0800

    video 5 custom error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+    return (
+        <div className="bg-black w-screen h-screen flex align-middle justify-center items-center">
+            <h1 className="text-center font-mono text-2xl text-white">404 | Halaman Tidak Ditemukan</h1>
+        </div>
+    )
+}
+
+export default Custom404
